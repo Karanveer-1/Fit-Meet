27bcbb24617f8c78a23bab3004a7840115b1dcce
tommyyeh0505 <tommyyeh0505@hotmail.com>
2018-11-07 12:02:28 -0800

    fix2

--- app/src/main/java/ca/bcit/fitmeet/MainActivity.java
+++ app/src/main/java/ca/bcit/fitmeet/MainActivity.java
@@ -8,7 +8,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
