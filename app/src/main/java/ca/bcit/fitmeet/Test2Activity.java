c82917f86b4f22ee04973734a1ff4286e1925cb5
tommyyeh0505 <32403423+tommyyeh0505@users.noreply.github.com>
2018-11-12 02:44:02 -0800

    implemented testActivity1 and testActivity2

--- app/src/main/java/ca/bcit/fitmeet/Test2Activity.java
+++ app/src/main/java/ca/bcit/fitmeet/Test2Activity.java
@@ -2,12 +2,80 @@ package ca.bcit.fitmeet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Test2Activity extends AppCompatActivity {
-
+    private ArrayList<String> testDataArray;
+    ListView listview;
+    DatabaseReference dref;
+    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test2);
+        dref = FirebaseDatabase.getInstance().getReference("testData");
+        testDataArray = new ArrayList<>();
+        testDataArray = retrieve();
+        listview=(ListView)findViewById(R.id.list_test_data);
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,testDataArray);
+        listview.setAdapter(adapter);
+    }
+
+    //READ
+    public ArrayList<String> retrieve()
+    {
+        dref.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                fetchData(dataSnapshot);
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        return testDataArray;
+    }
+
+    private void fetchData(DataSnapshot dataSnapshot)
+    {
+        String name = "";
+        for (DataSnapshot ds : dataSnapshot.getChildren())
+        {
+            String component = ds.getValue(String.class);
+
+            name = name +   component + " " ;
+        }
+        testDataArray.add(name);
+
     }
 }
--- app/src/main/java/ca/bcit/fitmeet/TestActivity.java
+++ app/src/main/java/ca/bcit/fitmeet/TestActivity.java
@@ -2,12 +2,64 @@ package ca.bcit.fitmeet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class TestActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private String userToken = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        mAuth = FirebaseAuth.getInstance();
+
     }
+
+
+    public void testAddData(View v){
+
+
+        EditText one =  findViewById(R.id.test_editText_1);
+        EditText two =  findViewById(R.id.test_editText_2);
+        EditText three =  findViewById(R.id.test_editText_3);
+
+        String oneString = one.getText().toString();
+        String twoString = two.getText().toString();
+        String threeString = three.getText().toString();
+
+        addTestData(oneString, twoString, threeString);
+
+    }
+
+
+    public void addTestData(String one, String two, String three){
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        userToken = currentUser.getUid();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("testData");
+        //This line is to allow duplicate data, by creating data under a new leaf node every time
+        DatabaseReference pushedPostRef = myRef.push();
+        String dataEntryID = pushedPostRef.getKey();
+        //Firebase takes a map to insert dat into its leaf nodes
+        HashMap<String, Object> testDataMap = new HashMap<>();
+        testDataMap.put("1", one);
+        testDataMap.put("2", two);
+        testDataMap.put("3", three);
+        //Puts map data into child of testData
+        myRef.child(dataEntryID).setValue(testDataMap);
+
+    }
+
+
 }
