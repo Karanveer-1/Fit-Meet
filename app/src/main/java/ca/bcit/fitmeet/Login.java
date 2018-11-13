4444df16930ce6e33f0408a9a4106c6e1cb9a685
tommyyeh0505 <32403423+tommyyeh0505@users.noreply.github.com>
2018-11-13 07:17:19 -0800

    Revert "Merge pull request #11 from Karanveer-1/revert-10-revert-7-ryan-dev"
    This reverts commit 154736813bbaa8499f0079d2d5845687ca2f6c48, reversing
    changes made to 9d397b90e75fe318b9adb79625e63a6380a3563f.

--- app/src/main/java/ca/bcit/fitmeet/Login.java
+++ app/src/main/java/ca/bcit/fitmeet/Login.java
@@ -13,6 +13,7 @@ import android.widget.Toast;
 import android.content.Context;
 import android.support.annotation.VisibleForTesting;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
 import com.google.android.gms.tasks.OnCompleteListener;
