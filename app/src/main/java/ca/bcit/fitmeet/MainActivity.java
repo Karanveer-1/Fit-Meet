d0abaf70aafdd6be486466c7ad7df11bc95038e9
tommyyeh0505 <32403423+tommyyeh0505@users.noreply.github.com>
2018-11-25 22:22:50 -0800

    added profile page functionality

--- app/src/main/java/ca/bcit/fitmeet/MainActivity.java
+++ app/src/main/java/ca/bcit/fitmeet/MainActivity.java
@@ -1,6 +1,7 @@
 package ca.bcit.fitmeet;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -113,5 +114,4 @@ public class MainActivity extends AppCompatActivity {
         }
         return false;
     }
-
 }
--- app/src/main/java/ca/bcit/fitmeet/ProfileFragment.java
+++ app/src/main/java/ca/bcit/fitmeet/ProfileFragment.java
@@ -1,21 +1,225 @@
 package ca.bcit.fitmeet;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.UUID;
+
+import ca.bcit.fitmeet.event.model.GlideApp;
+
+import static android.app.Activity.RESULT_OK;
 
 public class ProfileFragment extends Fragment {
 
+    private Button btnChoose, btnUpload;
+    private ImageView imageView;
+
+    DatabaseReference dref;
+    FirebaseStorage storage;
+    StorageReference storageReference;
+    private FirebaseAuth mAuth;
+    private String userToken;
+    private Uri filePath;
+    private String name;
+    private final int PICK_IMAGE_REQUEST = 71;
+    private View view;
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+    private ViewPager mViewPager;
     public ProfileFragment() {
-        // Required empty public constructor
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        name = "";
+        view = inflater.inflate(R.layout.fragment_profile, container, false);
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        userToken = currentUser.getUid();
+        imageView = (ImageView) view.findViewById(R.id.profile_picture);
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+        dref = FirebaseDatabase.getInstance().getReference("users");
+
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getChildFragmentManager());
+// Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) view.findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) view.findViewById(R.id.tabs);
+
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+
+        imageView.setClickable(true);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseImage();
+            }
+        });
+        TextView name = view.findViewById(R.id.profile_name);
+        imageView.setImageResource(R.drawable.placeholder);
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("profile").child(userToken);
+        Log.e("load", "loading");
+        GlideApp.with(this /* context */)
+                .load(storageReference)
+                .placeholder(R.drawable.placeholder)
+                .into(imageView);
+        getName();
+        return view;
     }
 
+
+    public void chooseImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.e("RESULT", "" + resultCode);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null) {
+            filePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), filePath);
+                imageView.setImageBitmap(bitmap);
+                Log.e("IMAGE", "SET");
+                uploadImage();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void uploadImage() {
+
+        if (filePath != null) {
+            final ProgressDialog progressDialog = new ProgressDialog(getActivity());
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+
+            StorageReference ref = storageReference.child("profile/" + userToken);
+            ref.putFile(filePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getActivity(), "Uploaded", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getActivity(), "Failed " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot
+                                    .getTotalByteCount());
+                            progressDialog.setMessage("Uploaded " + (int) progress + "%");
+                        }
+                    });
+        }
+    }
+
+
+    public void getName() {
+        ValueEventListener messageListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                name = "";
+
+                DataSnapshot dataSnapshot1 = dataSnapshot.child("firstName");
+                if (dataSnapshot1.exists()) {
+                    Log.e("KEY", dataSnapshot1.getKey());
+                    name += dataSnapshot1.getValue(String.class) + " " ;
+                }
+                DataSnapshot dataSnapshot2 = dataSnapshot.child("lastName");
+                if (dataSnapshot2.exists()) {
+                    Log.e("KEY", dataSnapshot2.getKey());
+                    name += dataSnapshot2.getValue(String.class);
+                }
+                TextView participantsTextView = view.findViewById(R.id.profile_name);
+                participantsTextView.setText(name);
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        };
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("users").child(userToken);
+        ref.addValueEventListener(messageListener);
+    }
+
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            Log.e("SECTION", ""+position);
+            return ProfileEventListFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+    }
 }
